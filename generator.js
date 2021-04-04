1878fbff7b783da3ded6549e698d6c2f642531be
veg <victoria.e.garcia87@gmail.com>
2021-04-04 16:33:57 -0700

    Saving progress.

---
+++ generator.js
@@ -0,0 +1,101 @@
+/* Message Randomizer
+A function that randomly generates quotes to motivate an individual when they are struggling through a project or work.
+*/
+
+// Random number of 0 - 19 to select random messages
+const random = Math.floor(Math.random() * 20);
+
+// Variable holding returned message
+const message = '';
+
+// Switch statement to evaluate random expressions.
+switch (random) {
+  case 0:
+    message = 'If you really believe in what you are doing, work hard, take nothing personal, and if something blocks one route, find another. Never give up! ~ Laurie Notaro';
+    break;
+  case 1:
+    message = 'I am not concerned that you have fallen. I am concerned that you arise. ~ Abraham Lincoln';
+    break;
+  case 2:
+    message = '';
+    break;
+  case 3:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 5:
+    message = '';
+    break;
+  case 6:
+    message = '';
+    break;
+  case 7:
+    message = '';
+    break;
+  case 8:
+    message = '';
+    break;
+  case 9:
+    message = '';
+    break;
+  case 10:
+    message = '';
+    break;
+  case 11:
+    message = '';
+    break;
+  case 12:
+    message = '';
+    break;
+  case 13:
+    message = '';
+    break;
+  case 14:
+    message = '';
+    break;
+  case 15:
+    message = '';
+    break;
+  case 16:
+    message = '';
+    break;
+  case 17:
+    message = '';
+    break;
+  case 18:
+    message = '';
+    break;
+  case 19:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+  case 4:
+    message = '';
+    break;
+
+}
